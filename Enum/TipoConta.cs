681da5e67fd290fed2bd6db8e7997f0f4f10a3dd
Maik Freitas <maikemir2003@gmail.com>
2021-05-23 21:59:28 -0300

    Adicionando Métodos e Interface de Comandos

---
+++ Enum/TipoConta.cs
@@ -0,0 +1,9 @@
+namespace Digital.IOBank
+
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,89 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Digital.IOBank
 {
     class Program
-    {
+    { 
+        static List<Conta> listaContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+			while (opcaoUsuario.ToUpper() != "X")
+			{
+				switch (opcaoUsuario)
+				{
+					case "1":
+						//ListarContas();
+						//break;
+					case "2":
+						InserirConta();
+						break;
+					case "3":
+						//Transferir();
+						break;
+					case "4":
+						//Sacar();
+						break;
+					case "5":
+						//Depositar();
+						break;
+                    case "C":
+						Console.Clear();
+						break;
+
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				opcaoUsuario = ObterOpcaoUsuario();
+			}
+			
+			Console.WriteLine("Obrigado por utilizar nossos serviços.");
+			Console.ReadLine();
+		}
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta");
+
+			Console.Write("Digite 1 para Conta Fisica ou 2 para Juridica: ");
+			int entradaTipoConta = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Nome do Cliente: ");
+			string entradaNome = Console.ReadLine();
+
+			Console.Write("Digite o saldo inicial: ");
+			double entradaSaldo = double.Parse(Console.ReadLine());
+
+			Console.Write("Digite o crédito: ");
+			double entradaCredito = double.Parse(Console.ReadLine());
+
+			Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+										saldo: entradaSaldo,
+										credito: entradaCredito,
+										nome: entradaNome);
+
+			listaContas.Add(novaConta);
+        }
+
+        private static string ObterOpcaoUsuario(){
+            Console.WriteLine();
+			Console.WriteLine("Digital IOBank a seu dispor!!!");
+			Console.WriteLine("Informe a opção desejada:");
+
+			Console.WriteLine("1- Listar contas");
+			Console.WriteLine("2- Inserir nova conta");
+			Console.WriteLine("3- Transferir");
+			Console.WriteLine("4- Sacar");
+			Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar Tela");
+			Console.WriteLine("X- Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
         }
     }
 }
