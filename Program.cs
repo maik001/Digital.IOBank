d15b2fb64eb13549b11838b203b8fe8cc4613490
Maik Freitas <maikemir2003@gmail.com>
2021-05-23 22:17:53 -0300

    Projeto Finalizado

--- Program.cs
+++ Program.cs
@@ -14,19 +14,19 @@ namespace Digital.IOBank
 				switch (opcaoUsuario)
 				{
 					case "1":
-						//ListarContas();
-						//break;
+						ListarContas();
+						break;
 					case "2":
 						InserirConta();
 						break;
 					case "3":
-						//Transferir();
+						Transferir();
 						break;
 					case "4":
-						//Sacar();
+						Sacar();
 						break;
 					case "5":
-						//Depositar();
+						Depositar();
 						break;
                     case "C":
 						Console.Clear();
@@ -42,6 +42,61 @@ namespace Digital.IOBank
 			Console.WriteLine("Obrigado por utilizar nossos serviços.");
 			Console.ReadLine();
 		}
+
+        private static void Depositar()
+		{
+			Console.Write("Digite o número da conta: ");
+			int indiceConta = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o valor a ser depositado: ");
+			double valorDeposito = double.Parse(Console.ReadLine());
+
+            listaContas[indiceConta].Depositar(valorDeposito);
+		}
+
+		private static void Sacar()
+		{
+			Console.Write("Digite o número da conta: ");
+			int indiceConta = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o valor a ser sacado: ");
+			double valorSaque = double.Parse(Console.ReadLine());
+
+            listaContas[indiceConta].Sacar(valorSaque);
+		}
+
+		private static void Transferir()
+		{
+			Console.Write("Digite o número da conta de origem: ");
+			int indiceContaOrigem = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número da conta de destino: ");
+			int indiceContaDestino = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o valor a ser transferido: ");
+			double valorTransferencia = double.Parse(Console.ReadLine());
+
+            listaContas[indiceContaOrigem].Transferir(valorTransferencia, listContas[indiceContaDestino]);
+		}
+
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar contas");
+
+			if (listaContas.Count == 0)
+			{
+				Console.WriteLine("Nenhuma conta cadastrada.");
+				return;
+			}
+
+			for (int i = 0; i < listaContas.Count; i++)
+			{
+				Conta conta = listaContas[i];
+				Console.Write("#{0} - ", i);
+				Console.WriteLine(conta);
+			}
+        }
 
         private static void InserirConta()
         {
